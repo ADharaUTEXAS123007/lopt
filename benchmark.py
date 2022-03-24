2aaafaffddb70464bd596f5c7fbfa91e610269a4
Arnab Dhara <adhara@IG-A03506.local>
2022-03-24 10:37:39 -0500

    requires_grad

--- benchmark.py
+++ benchmark.py
@@ -70,7 +70,7 @@ def avoObj():
             reflect = torch.unsqueeze(reflect,dim=0)
             synth = conv1d(reflect, wavelet, padding=int(wavelet.shape[-1] / 2))
 
-        return F.mse_loss(synth, dobs)
+        return zpall
         # tr1 = zpall*0
         # reflectivity = zpall[:-1,:]*0
         
