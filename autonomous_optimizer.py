a92b5c4dc48952ea3511b723decc4e087c8d93ca
Arnab Dhara <adhara@dhcp41.ig.utexas.edu>
2022-03-30 15:51:56 -0500

    opt

--- autonomous_optimizer.py
+++ autonomous_optimizer.py
@@ -14,7 +14,7 @@ def make_observation(obj_value, obj_values, gradients, current_values, num_param
     # as the ith previous gradient.
     dobs = np.load('seis.npy')
     print("shape of dobs :", np.shape(dobs))
-    #current_values = current_values.detach().numpy()
+    current_values = current_values.numpy()
     print("current values :", current_values)
     observation = np.zeros((history_len, 1 + num_params), dtype="float32")
     observation[: len(obj_values), 0] = (
