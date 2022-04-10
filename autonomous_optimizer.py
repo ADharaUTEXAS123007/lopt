2e68128ece15fbd55365ec9c4802d7671ecb39c5
Arnab Dhara <adhara@IG-A03506.local>
2022-04-10 15:14:56 -0500

    lopt

--- autonomous_optimizer.py
+++ autonomous_optimizer.py
@@ -30,9 +30,9 @@ def make_observation(obj_value, obj_values, gradients, current_values, num_param
     observation /= 1
     #print("shape of observation :", np.shape(observation))
     observation = np.transpose(dobs)
-    current_values = (current_values - np.mean(current_values))/np.std(current_values)
     print("mean of current values :", np.mean(current_values))
     print("std of current values :", np.std(current_values))
+    current_values = (current_values - np.mean(current_values))/np.std(current_values)
     observation = np.append(observation,current_values,axis=0)
     #observation = np.expand_dims(observation, axis=0)
     #observation = current_values
